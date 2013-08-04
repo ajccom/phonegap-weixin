d495b947932efeae3a613b035ac4bac833f2f6dd
陈健 <136157536@qq.com>
2013-08-04 10:53:25 +0800

    phonegap weixin plugin
    phonegap weixin plugin

---
+++ android/src/com/phonegap/plugins/weixin/WeiXin.java
@@ -0,0 +1,337 @@
+package com.phonegap.plugins.weixin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URL;
+
+import com.tencent.mm.sdk.openapi.BaseReq;
+import com.tencent.mm.sdk.openapi.BaseResp;
+import com.tencent.mm.sdk.openapi.ConstantsAPI;
+import com.tencent.mm.sdk.openapi.ShowMessageFromWX;
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.IWXAPIEventHandler;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+import com.tencent.mm.sdk.openapi.WXAppExtendObject;
+import com.tencent.mm.sdk.openapi.SendMessageToWX;
+import com.tencent.mm.sdk.openapi.WXEmojiObject;
+import com.tencent.mm.sdk.openapi.WXImageObject;
+import com.tencent.mm.sdk.openapi.WXMediaMessage;
+import com.tencent.mm.sdk.openapi.WXMusicObject;
+import com.tencent.mm.sdk.openapi.WXTextObject;
+import com.tencent.mm.sdk.openapi.WXVideoObject;
+import com.tencent.mm.sdk.openapi.WXWebpageObject;
+import com.tencent.mm.sdk.openapi.SendAuth;
+import com.example.easynotepad.Util;
+
+import [yourPackage].R;
+
+import org.apache.cordova.api.CallbackContext;
+import org.apache.cordova.api.CordovaPlugin;
+import org.apache.cordova.api.LOG;
+import org.apache.cordova.api.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.content.DialogInterface;
+import android.content.ContextWrapper;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+
+public class WeiXin extends CordovaPlugin implements IWXAPIEventHandler {
+	
+	private static final String SDCARD_ROOT = Environment.getExternalStorageDirectory().getAbsolutePath();
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) {
+		boolean result = false;
+        try {
+            if (action.equals("register")) {
+            	String appId = args.getString(0); 
+				this.register(appId);
+				callbackContext.success();
+                result = true;
+            } else if (action.equals("unregister")) { 
+				this.unregister();
+				callbackContext.success();
+                result = true;
+            }
+            else if (action.equals("openWeixin")) {
+				this.open();
+				callbackContext.success();
+				result = true;
+            }
+			else if (action.equals("send")) {
+				JSONObject cfg = args.getJSONObject(0);
+				if (cfg.getString("type").equals("text")) {
+					this.sendText(cfg.getString("text"), cfg.getBoolean("isSendToTimeline"));
+				} else if (cfg.getString("type").equals("image")) {
+					this.sendImage(cfg.getString("data"), cfg.getString("imageType"), cfg.getBoolean("isSendToTimeline"));
+				} else if (cfg.getString("type").equals("music")) {
+					this.sendMusic(cfg.getString("url"), cfg.getBoolean("isLowBand"), cfg.getString("title"), cfg.getString("desc"), cfg.getBoolean("isSendToTimeline"));
+				} else if (cfg.getString("type").equals("video")) {
+					this.sendVideo(cfg.getString("url"), cfg.getBoolean("isLowBand"), cfg.getString("title"), cfg.getString("desc"), cfg.getBoolean("isSendToTimeline"));
+				} else if (cfg.getString("type").equals("webpage")) {
+					this.sendWebPage(cfg.getString("url"), cfg.getString("title"), cfg.getString("desc"), cfg.getBoolean("isSendToTimeline"));
+				}
+				
+				callbackContext.success();
+				result = true;
+            }
+            else {
+                result = false;
+            }
+        } catch (JSONException e) {
+            callbackContext.error("JSON Exception");
+			result = false;
+        }
+		return result;
+    }
+	
+	private IWXAPI api;
+	
+	public void register(String appId) {
+		Context Activity = this.cordova.getActivity().getApplicationContext();
+		api = WXAPIFactory.createWXAPI(Activity, appId, false);		
+		api.registerApp(appId);
+	}
+	
+	public void open() {
+		api.openWXApp();
+	}
+
+	
+	//send text
+	public void sendText(String text, boolean isSendToTimeline) {
+		
+		try{
+			
+			WXTextObject textObj = new WXTextObject();
+			textObj.text = text;
+			
+			WXMediaMessage msg = new WXMediaMessage();
+			msg.mediaObject = textObj;
+			msg.description = text;
+
+			SendMessageToWX.Req req = new SendMessageToWX.Req();
+			req.transaction = String.valueOf(System.currentTimeMillis());//buildTransaction("text");
+			req.message = msg;
+			int wxSdkVersion = api.getWXAppSupportAPI();
+			if (wxSdkVersion >= 0x21020001) {
+				req.scene = isSendToTimeline ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+			} else {
+				req.scene = SendMessageToWX.Req.WXSceneSession;
+			}
+			api.sendReq(req);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//send image
+	public void sendImage(String data, String imageType, boolean isSendToTimeline) {
+		
+		File file = null;
+		String path = "";
+		Bitmap bmp = null;
+		try{
+			WXImageObject imgObj = new WXImageObject();
+			if (imageType.equals("url")) {
+				imgObj.imageUrl = data;
+				bmp = BitmapFactory.decodeStream(new URL(data).openStream());
+				
+			} else if (imageType.equals("path")) {
+				LOG.d("WeChat Plugin", "file path: " + SDCARD_ROOT + data);
+				path = (SDCARD_ROOT + data).replaceAll(" ", "%20");
+				file = new File(path);
+				if (!file.exists()) {
+					LOG.d("WeChat Plugin", "file not exists");
+					return;
+				} else {
+					LOG.d("WeChat Plugin", "get file @" + path);
+				}
+				imgObj.setImagePath(path); 
+				
+				FileInputStream fis = new FileInputStream(path);
+				bmp = BitmapFactory.decodeStream(fis);
+				//bmp = BitmapFactory.decodeFile(path);
+			}
+			
+			WXMediaMessage msg = new WXMediaMessage();
+			msg.mediaObject = imgObj;
+			
+			Bitmap thumbBmp = Bitmap.createScaledBitmap(bmp, 150, 150, true);
+			bmp.recycle();
+			msg.thumbData = Util.bmpToByteArray(thumbBmp, true);
+			
+			SendMessageToWX.Req req = new SendMessageToWX.Req();
+			req.transaction = String.valueOf(System.currentTimeMillis());
+			req.message = msg;
+			int wxSdkVersion = api.getWXAppSupportAPI();
+			if (wxSdkVersion >= 0x21020001) {
+				req.scene = isSendToTimeline ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+			} else {
+				req.scene = SendMessageToWX.Req.WXSceneSession;
+			}
+			api.sendReq(req);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//send music
+	public void sendMusic(String url, boolean isLowBand, String title, String desc, boolean isSendToTimeline) {
+		WXMusicObject music = new WXMusicObject();
+		if (isLowBand == false) {
+			music.musicUrl = url;
+		} else {
+			music.musicLowBandUrl = url;
+		}
+		//"http://staff2.ustc.edu.cn/~wdw/softdown/index.asp/0042515_05.ANDY.mp3";
+		//music.musicUrl="http://120.196.211.49/XlFNM14sois/AKVPrOJ9CBnIN556OrWEuGhZvlDF02p5zIXwrZqLUTti4o6MOJ4g7C6FPXmtlh6vPtgbKQ==/31353278.mp3";
+
+		WXMediaMessage msg = new WXMediaMessage();
+		msg.mediaObject = music;
+		msg.title = title;
+		msg.description = desc;
+
+		Context Activity = this.cordova.getActivity().getApplicationContext();
+		Bitmap thumb = BitmapFactory.decodeResource(Activity.getResources(), R.drawable.music);
+		msg.thumbData = Util.bmpToByteArray(thumb, true);
+
+		SendMessageToWX.Req req = new SendMessageToWX.Req();
+		req.transaction = buildTransaction("music");
+		req.message = msg;
+		int wxSdkVersion = api.getWXAppSupportAPI();
+		if (wxSdkVersion >= 0x21020001) {
+			req.scene = isSendToTimeline ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+		} else {
+			req.scene = SendMessageToWX.Req.WXSceneSession;
+		}
+		api.sendReq(req);
+		
+	}
+		
+	//send video
+	public void sendVideo(String url, boolean isLowBand, String title, String desc, boolean isSendToTimeline) {
+ 		WXVideoObject video = new WXVideoObject();
+		if (isLowBand == false) {
+			video.videoUrl = url;
+		} else {
+			video.videoLowBandUrl = url;
+		}
+		
+		WXMediaMessage msg = new WXMediaMessage(video);
+		msg.title = title;
+		msg.description = desc;
+
+		Context Activity = this.cordova.getActivity().getApplicationContext();
+		Bitmap thumb = BitmapFactory.decodeResource(Activity.getResources(), R.drawable.video);
+		msg.thumbData = Util.bmpToByteArray(thumb, true);
+
+		SendMessageToWX.Req req = new SendMessageToWX.Req();
+		req.transaction = buildTransaction("video");
+		req.message = msg;
+		int wxSdkVersion = api.getWXAppSupportAPI();
+		if (wxSdkVersion >= 0x21020001) {
+			req.scene = isSendToTimeline ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+		} else {
+			req.scene = SendMessageToWX.Req.WXSceneSession;
+		}
+		api.sendReq(req);
+		//LOG.d("!!!!!!!!!!!!!!!!!!!!", video.videoUrl);
+		
+	}
+	
+	//send webpage
+	public void sendWebPage(String url, String title, String desc, boolean isSendToTimeline) {
+		WXWebpageObject webpage = new WXWebpageObject();
+		webpage.webpageUrl = url;
+		WXMediaMessage msg = new WXMediaMessage(webpage);
+		msg.title = title;
+		msg.description = desc;
+		Context Activity = this.cordova.getActivity().getApplicationContext();
+		Bitmap thumb = BitmapFactory.decodeResource(Activity.getResources(), R.drawable.webpage);
+		msg.thumbData = Util.bmpToByteArray(thumb, true);
+		
+		SendMessageToWX.Req req = new SendMessageToWX.Req();
+		req.transaction = buildTransaction("webpage");
+		req.message = msg;
+		int wxSdkVersion = api.getWXAppSupportAPI();
+		if (wxSdkVersion >= 0x21020001) {
+			req.scene = isSendToTimeline ? SendMessageToWX.Req.WXSceneTimeline : SendMessageToWX.Req.WXSceneSession;
+		} else {
+			req.scene = SendMessageToWX.Req.WXSceneSession;
+		}
+		api.sendReq(req);
+		LOG.d("!!!!!!!!!!!!!!!!!!!!", webpage.webpageUrl);
+	}
+	
+	//unregister
+	public void unregister() {
+		api.unregisterApp();
+	}
+	
+	// 微信发送请求到第三方应用时，会回调到该方法
+		@Override
+		public void onReq(BaseReq req) {
+			LOG.d("req", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			switch (req.getType()) {
+			case ConstantsAPI.COMMAND_GETMESSAGE_FROM_WX:
+				LOG.d("onReq", "COMMAND_GETMESSAGE_FROM_WX");
+				System.out.println(ConstantsAPI.COMMAND_GETMESSAGE_FROM_WX);
+				break;
+			case ConstantsAPI.COMMAND_SHOWMESSAGE_FROM_WX:
+				LOG.d("onReq", "COMMAND_SHOWMESSAGE_FROM_WX");
+				System.out.println(ConstantsAPI.COMMAND_SHOWMESSAGE_FROM_WX);
+				break;
+			default:
+				break;
+			}
+		}
+
+		// 第三方应用发送到微信的请求处理后的响应结果，会回调到该方法
+		@Override
+		public void onResp(BaseResp resp) {
+			LOG.d("resp", "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+			int result = 0;
+			
+			switch (resp.errCode) {
+			case BaseResp.ErrCode.ERR_OK:
+				result = 1;
+				break;
+			case BaseResp.ErrCode.ERR_USER_CANCEL:
+				result = 2;
+				break;
+			case BaseResp.ErrCode.ERR_AUTH_DENIED:
+				result = 3;
+				break;
+			default:
+				result = 4;
+				break;
+			}
+			LOG.d("onResp", "result");
+			System.out.println(result);
+		}
+
+
+
+
+
+
+		private String buildTransaction(final String type) {
+			return (type == null) ? String.valueOf(System.currentTimeMillis()) : type + System.currentTimeMillis();
+		}
+}
---
+++ android/weixin.js
@@ -0,0 +1,41 @@
+/**
+ * WeiXin
+ * version 0.9
+ * author: Jian Chen / ajccom
+ * https://github.com/ajccom/phonegap-weixin
+ * tested cordova version - 2.3.0
+ **/
+(function(cordova){
+	function WeiXin() {};
+
+	WeiXin.prototype.register = function(appid, success, error) {
+		cordova.exec(success, error, "WeiXin", "register", [appid]);
+	};
+	
+	WeiXin.prototype.unregister = function(success, error) {
+		cordova.exec(success, error, "WeiXin", "unregister", []);
+	};
+	
+	WeiXin.prototype.openWeixin = function(success, error) {
+		cordova.exec(success || null, error || null, "WeiXin", "openWeixin", []);
+	};
+	
+	WeiXin.prototype.send = function(arg, success, error) {
+		if (typeof arg.isSendToTimeline === 'undefined') {arg.isSendToTimeline = true}
+		if (arg.type === 'image' && typeof arg.imageType === 'undefined') {arg.imageType = 'url'}
+		if (arg.type === 'music' || arg.type === 'video' || arg.type === 'webpage') {
+			if (typeof arg.title === 'undefined') {arg.title = ''}
+			if (typeof arg.desc === 'undefined') {arg.desc = ''}
+			if (typeof arg.isLowBand === 'undefined') {arg.isLowBand = false}
+		}
+		cordova.exec(success, error, "WeiXin", "send", [arg]);
+	};
+	
+	if(!window.plugins) {
+		window.plugins = {};
+	}
+	if (!window.plugins.weixin) {
+		window.plugins.weixin = new WeiXin();
+	}
+	
+})(window.PhoneGap || window.Cordova || window.cordova);
